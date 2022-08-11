6349e09c99a02ce3767b9208776ea1165ba13bac
Eeshan8503 <eeshanmattey@gmail.com>
2022-08-12 00:55:11 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+from flask_mysql_connector import MySQL
+#
+app = Flask(__name__)
+#
+# app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = ''
+
+mysql = MySQL(app)
+@app.route('/')
+def home():
+    cur = mysql.new_cursor(dictionary=True)
+    cur.execute('CREATE DATABASE DBMS_PROJ')
+    return "hello world"
+
+if __name__ == "__main__":
+    app.run(debug=True)
